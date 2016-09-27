d32f529f6a9e985f3ee50a9b1c917d291eb14b5a
delasalle-sio-bienvenu-e <delasalle.sio.bienvenu.e@gmail.com>
2016-09-27 17:20:58 +0200

    Modifications ConsulterSalles
    - Controleur terminé
    - Vue consulter salle à modifier en entier

--- controleurs/CtrlConsulterSalles.php
+++ controleurs/CtrlConsulterSalles.php
@@ -1,1 +1,37 @@
 <?php
+// Projet Réservations M2L - version web mobile
+// fichier : controleurs/CtrlConsulterSalles.php
+// Rôle : traiter la demande de consultation des réservations d'un utilisateur
+// écrit par 27/09/2016 par ERWANN BIENVENU
+// modifié par 27/09/2016 par ERWANN BIENVENU
+
+// on vérifie si le demandeur de cette action est bien authentifié
+if ( $_SESSION['niveauUtilisateur'] != 'utilisateur' && $_SESSION['niveauUtilisateur'] != 'administrateur') {
+	// si le demandeur n'est pas authentifié, il s'agit d'une tentative d'accès frauduleux
+	// dans ce cas, on provoque une redirection vers la page de connexion
+	header ("Location: index.php?action=Deconnecter");
+}
+else {
+	// connexion du serveur web à la base MySQL
+	include_once ('modele/DAO.class.php');
+	$dao = new DAO();
+	
+	// récupération des salles libres depuis la classe DAO
+	$lesReservations = $dao->getLesSalles();
+
+	// mémorisation du nombre de salles libres
+	$nbReponses = sizeof($lesSalles);
+
+	// préparation d'un message précédent la liste
+	if ($nbReponses == 0) {
+		$message = "Il n'y a aucune salle disponible !";
+	}
+	else {
+		$message = $nbReponses . " salle(s) disponibles en réservation :";
+	}
+
+	// affichage de la vue
+	include_once ('vues/VueConsulterSalles.php');
+
+	unset($dao);		// fermeture de la connexion à MySQL
+}
--- vues/VueConsulterSalles.php
+++ vues/VueConsulterSalles.php
@@ -1,1 +1,57 @@
 <?php
+// Projet Réservations M2L - version web mobile
+// fichier : vues/VueConsulterSalles.php
+// Rôle : visualiser la liste des salles libres
+// cette vue est appelée par le contôleur controleurs/CtrlConsulterSalles.php
+// Création : 27/09/2016 par ERWANN BIENVENU
+// Mise à jour : 27/09/2016 par ERWANN BIENVENU
+?>
+<!doctype html>
+<html>
+	<head>
+		<?php include_once ('vues/head.php'); ?>
+	</head>
+	 
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="<?php echo $themeNormal; ?>">
+				<h4>M2L-GRR</h4>
+				<a href="index.php?action=Menu" data-transition="<?php echo $transition; ?>">Retour menu</a>
+			</div>
+			<div data-role="content">
+				<h4 style="text-align: center; margin-top: 0px; margin-bottom: 0px;"><?php echo $message;?></h4>
+				<p style="text-align: center;"><?php echo $message; ?></p>
+				<ul data-role="listview" style="margin-top: 5px;">
+				<?php
+				// Avec jQuery Mobile, les réservations sont affichées à l'aide d'une liste <ul>
+				// chaque réservation est affichée à l'aide d'un élément de liste <li>
+				// chaque élément de liste <li> peut contenir des titres et des paragraphes
+
+				foreach ($lesSalles as $uneSalle)
+				{ ?>
+					<li><a href="#">
+					<h5>Réserv. n° <?php echo $uneReservation->getId(); ?></h5>
+					<p>Passée le <?php echo Outils::convertirEnDateFR(substr($uneReservation->getTimestamp(), 0, 10)); ?></p>
+					<p>Début : <?php echo date('d/m/Y H:i:s', $uneReservation->getStart_time()); ?></p>
+					<p>Fin : <?php echo date('d/m/Y H:i:s', $uneReservation->getEnd_time()); ?></p>
+					<p>Salle : <?php echo $uneReservation->getRoom_name(); ?></p>
+					<p>Etat : <?php if ($uneReservation->getStatus() == 0) 
+										echo 'confirmée';
+					 				else 
+					 					echo 'provisoire';?></p>
+					<?php if ($uneReservation->getStatus() == 0) 
+							// la classe "ui-li-aside" de JQuery Mobile permet de positionner un élément à droite
+							echo '<h5 class="ui-li-aside">Digicode ' . $uneReservation->getDigicode() . '</h5>';?>
+					</a></li>
+				<?php
+				} ?>
+				</ul>
+
+			</div>
+			<div data-role="footer" data-position="fixed" data-theme="<?php echo $themeNormal;?>">
+				<h4>Suivi des réservations de salles<br>Maison des ligues de Lorraine (M2L)</h4>
+			</div>
+		</div>
+		
+	</body>
+</html>
